4f9641de1e4ef6cf28a6fecf15e3f03263c1911c
João Vitor <joao.dev02@outlook.com>
2024-04-21 12:21:03 -0300

    Commit inicial

---
+++ src/test/java/com/joaodev/propostaapp/PropostaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.propostaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
